2313d8003a80fb2e6c52ea62b36ee3347bb6eb79
Kamil Duliniec <123dulko@wp.pl>
2021-10-12 21:55:43 +0200

    order files structure

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import translator from "./translator.js";
-import worldLive from "./worldLive.js";
+import translator from "./app/translator.js";
+import worldLive from "./app/worldLive.js";
 
-const firstTextArea = document.querySelector('#postText');
+const firstTextArea = document.querySelector("#postText");
 
-window.onload = function(){
-    firstTextArea.focus()
-}
-
+window.onload = function () {
+  firstTextArea.focus();
+};
 
 const worldMehtods = worldLive();
 worldMehtods.inPutForm();
